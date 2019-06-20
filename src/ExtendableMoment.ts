63d919671b497c05b8a68c23ead8b0c06091a85d
Hugh Grigg <hugh@freetrade.io>
2019-06-20 11:17:54 +0100

    v1.0.2

--- src/ExtendableMoment.ts
+++
@@ -1,8 +0,0 @@
-import moment = require("moment")
-
-// @ts-ignore
-export class ExtendableMoment implements moment.Moment {}
-
-// noinspection JSPotentiallyInvalidConstructorUsage
-Object.setPrototypeOf(ExtendableMoment.prototype, moment.prototype)
-Object.setPrototypeOf(ExtendableMoment, moment)
